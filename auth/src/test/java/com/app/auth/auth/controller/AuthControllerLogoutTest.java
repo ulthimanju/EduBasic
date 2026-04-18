3213980ac08d980b9834072b4a3742a6ff2ddb35
ULTHI MANJUNATH <umanjunath2003@gmail.com>
2026-04-18 08:18:11 +0530

    refactor: update deprecated OAuth2ClientAutoConfiguration path

--- auth/src/test/java/com/app/auth/auth/controller/AuthControllerLogoutTest.java
+++ auth/src/test/java/com/app/auth/auth/controller/AuthControllerLogoutTest.java
@@ -40,7 +40,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
         excludeAutoConfiguration = {
                 org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class,
                 org.springframework.boot.autoconfigure.security.servlet.SecurityFilterAutoConfiguration.class,
-                org.springframework.boot.autoconfigure.security.oauth2.client.servlet.OAuth2ClientAutoConfiguration.class,
+                org.springframework.boot.autoconfigure.security.oauth2.client.OAuth2ClientAutoConfiguration.class,
                 org.springframework.boot.autoconfigure.security.oauth2.client.servlet.OAuth2ClientWebSecurityAutoConfiguration.class
         })
 class AuthControllerLogoutTest {
